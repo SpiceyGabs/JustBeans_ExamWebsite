67ee78d779024c0ce923db0fef1ea798ee23af4d
SpiceyGabs <sgsquared.2@gmail.com>
2025-11-02 04:11:44 +0200

    hamburger css and html

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LOGINNNNN SCRIPPPTTT , please dont forget!!!!!
 const form = document.getElementById ('form');
 const username = document.getElementById ('username');
 const email = document.getElementById ('email');
