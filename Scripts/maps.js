0293d92f1e0ded8ec19db3126f764f42a868ee11
SpiceyGabs <sgsquared.2@gmail.com>
2025-11-08 21:00:36 +0200

    map 2

--- Scripts/maps.js
+++ Scripts/maps.js
@@ -19,3 +19,26 @@ origins.forEach(origin => {
     .addTo(map)
     .bindPopup(`<b>${origin.name}</b><br>Source of our beans`);
 });
+
+//FOR MAP 2: LOCAL MAP
+const map2 = L.map('worldMap2').setView([-28.4793, 24.6727], 5); // centered roughly over South Africa
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 12,
+}).addTo(map2);
+
+const distributors = [
+  { name: "Woolworths Sandton City Mall", coords: [-26.1074, 28.0567] },
+  { name: "Woolworths Bryanston", coords: [-26.0563, 28.0166] },
+  { name: "Spar Hartebeespoort", coords: [-25.7339, 27.8903] },
+  { name: "Woolworths Menlyn Mall", coords: [-25.7843, 28.2775] },
+  { name: "Woolworths Knysna", coords: [-34.0358, 23.0469] },
+  { name: "Woolworths V&A Waterfront", coords: [-33.9076, 18.4206] }
+];
+
+//Place markers 
+distributors.forEach(store => {
+  L.marker(store.coords)
+    .addTo(map2)
+    .bindPopup(`<b>${store.name}</b><br>JustBeans stocked here!`);
+});
